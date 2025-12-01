0a0e7b822f541ad6f3a9e691d87597cd238d8c4a
Park ha <115542242+parkha6@users.noreply.github.com>
2025-12-01 12:19:22 +0900

    feat 모노싱글톤(채한승 튜터님것.)

---
+++ Assets/01.Script/Managers/MonoSingleton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+/// <summary>
+/// 싱글턴 패턴 배치용 클래스
+/// </summary>
+public class MonoSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            { SetupInstance(); }
+            return instance;
+        }
+    }
+    private static void SetupInstance()
+    {
+        instance = FindAnyObjectByType<T>();
+        if (instance == null)
+        {
+            instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            DontDestroyOnLoad(instance.gameObject);
+        }
+    }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        { Destroy(Instance.gameObject); }
+        else
+        { DontDestroyOnLoad(this.gameObject); }
+    }
+}
